17862555d8ba36f87a4db2e9ec6232dc173ecd81
DavideSias <davidesias77@gmail.com>
2022-12-14 14:37:20 +0100

    Add snack 1

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
+<?php
+//Snack 1
+
+$arrBasket = [
+    [
+        [
+            'casa' => 'Olimpia Milano',
+            'punteggio' => '55',
+        ],
+        [
+            'ospite' => 'Cantù',
+            'punteggio' => '60',
+        ],
+    ],
+    [
+        [
+            'casa' => 'Dinamo Sassari',
+            'punteggio' => '80',
+        ],
+        [
+            'ospite' => 'Venezia',
+            'punteggio' => '78',
+        ],
+    ],
+    [
+        [
+            'casa' => 'Bologna',
+            'punteggio' => '96',
+        ],
+        [
+            'ospite' => 'Roma',
+            'punteggio' => '101',
+        ],
+    ],
+    [
+        [
+            'casa' => 'Lecce',
+            'punteggio' => '63',
+        ],
+        [
+            'ospite' => 'Verona',
+            'punteggio' => '65',
+        ],
+    ],
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +54,14 @@
     <title>PHP snacks</title>
 </head>
 <body>
-    
+    <?= '<pre>' . print_r($arrBasket, true) . '</pre>' ?>
+    <h1>Snack 1</h1>
+    <ul><?php 
+            $arrBasketLength = count($arrBasket);
+            for ($i = 0; $i < $arrBasketLength; $i++) { ?>
+                <li><?= $arrBasket[$i][0]['casa'] . ' - ' . $arrBasket[$i][1]['ospite'] . ' | ' . $arrBasket[$i][0]['punteggio'] . ' - ' . $arrBasket[$i][1]['punteggio']?></li><?php
+            }
+        ?>
+    </ul>
 </body>
 </html>
