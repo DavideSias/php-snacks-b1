522754443c98521019e3c2df768488b1a9b544d1
DavideSias <davidesias77@gmail.com>
2022-12-14 15:42:39 +0100

    Add snack 4

--- index.php
+++ index.php
@@ -43,6 +43,47 @@ $arrBasket = [
         ],
     ],
 ];
+
+//Snack 2
+
+// if(isset($_GET['mail'], $_GET['name'], $_GET['age'] )){
+//     $mail = $_GET['mail'];
+//     $name = $_GET['name'];
+//     $age = $_GET['age'];
+
+//     if((strpos($mail, '@') !== FALSE) && (strpos($mail, '.' !== FALSE))){
+
+//         if(strlen($name) < 2) {
+//             $message = 'Il nome deve essere più lungo di 3 caratteri';
+//         } else {
+//             $message = 'Accesso Eseguito';
+//         };
+
+//     } else {
+//         $message = "La mail deve contenere '@' e '.'";
+//     };
+
+// } else {
+//     $mail = '';
+//     $name = '';
+//     $message = 'Inserire tutti i dati richiesti';
+// }
+   
+//Snack 4
+
+$numArray = [];
+$i = 0;
+
+while ($i < 15) {
+    $randomNumber = rand(0 , 100);
+
+    if(!in_array($randomNumber, $numArray)) {
+        $numArray[] = $randomNumber;
+        $i++;
+    }
+} 
+
+
 ?>
 
 <!DOCTYPE html>
@@ -54,12 +95,36 @@ $arrBasket = [
     <title>PHP snacks</title>
 </head>
 <body>
-    <?= '<pre>' . print_r($arrBasket, true) . '</pre>' ?>
+    <!-- <?= '<pre>' . print_r($arrBasket, true) . '</pre>' ?> -->
     <h1>Snack 1</h1>
     <ul><?php 
             $arrBasketLength = count($arrBasket);
             for ($i = 0; $i < $arrBasketLength; $i++) { ?>
                 <li><?= $arrBasket[$i][0]['casa'] . ' - ' . $arrBasket[$i][1]['ospite'] . ' | ' . $arrBasket[$i][0]['punteggio'] . ' - ' . $arrBasket[$i][1]['punteggio']?></li><?php
+            }
+        ?>
+    </ul>
+
+    <!-- <h1>Snack 2</h1>
+    <form action="" method="get">
+        <label for="mail">Mail: </label>
+        <input type="text" name="mail" id="mail">
+        <label for="name">Nome: </label>
+        <input type="text" name="name" id="name">
+        <label for="age">Età: </label>
+        <input type="number" name="age" id="age">
+        <button>Check info</button>
+    </form>
+
+    <h2><?= $message ?></h2> -->
+
+    <h1>Snack 4</h1>
+
+    <!-- <?= '<pre>' . print_r($numArray, true) . '</pre>' ?>  -->
+    <ul><?php 
+            $numArrayLength = count($numArray);
+            for ($i = 0; $i < $numArrayLength; $i++) { ?>
+                <li><?= $numArray[$i]?></li><?php
             }
         ?>
     </ul>
